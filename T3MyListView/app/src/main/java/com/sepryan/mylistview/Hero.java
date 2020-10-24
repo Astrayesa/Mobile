f3e33ea73cb348b3b9bffe0893ab84a9f74ba28a
Astrayesa <jsparrow0404@gmail.com>
2020-10-24 08:59:53 +0700

    Modul 5 Library

---
+++ T3MyListView/app/src/main/java/com/sepryan/mylistview/Hero.java
@@ -0,0 +1,31 @@
+package com.sepryan.mylistview;
+
+public class Hero {
+    private int photo;
+    private String name;
+    private String description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
